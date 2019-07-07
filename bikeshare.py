82abf0aa804d514c666922984fe0b2b6ed7be840
Junko Yanagi <yanagijunko@gmail.com>
2019-07-07 20:44:21 +0700

    Update City data explanation in python file

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+## City data is here
 CITY_DATA = { 'Chicago': 'chicago.csv',
               'New York': 'new_york_city.csv',
               'Washington': 'washington.csv' }
